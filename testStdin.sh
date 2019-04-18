cea2c2068f4ce4c7909295c6a2d5621a5f7004f1
Phil Wright- Christie <philwc@gmail.com>
2019-04-18 13:15:32 +0100

    Remove test file

--- testStdin.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-echo ../megatron/controllers/AjaxInventoryController.php | php -dxdebug.remote_enable=1 -dxdebug.remote_mode=req -dxdebug.remote_port=9000 -dxdebug.remote_host=127.0.0.10 ./bin/phpdoccheck --from-stdin
